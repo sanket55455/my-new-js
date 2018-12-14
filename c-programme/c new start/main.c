eb6326e14a9ca56ed934c9081255265b6289fcde
Sanket Balasaheb Pawar <sanketpawarsp56@gmail.com>
2018-12-14 12:28:06 +0530

    c: adding c programs

---
+++ c-programme/c new start/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   /* printf("Hello world!\n");
+    printf("Hello world!\n");*/
+
+
+    int a,b, result=0;
+    printf("enter the value of a and b:");
+  scanf("%d%d",&a,&b);
+    result=a+b;
+    printf("result:%d\n",result);
+
+
+   /* int a=10;
+    int b=a++;//
+
+    int c=(a++)+(b++);//
+    int d=(++b)+(a++)+(++c);//46
+    printf("a:%d",a);//a=13
+    printf("b:%d",b);//b=12
+    printf("c:%d",c);//c=22
+    printf("d:%d",d);//d=46*/
+
+
+
+
+    return 0;
+
+
+
+}
---
+++ c-programme/practice/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+/*int number;
+printf("5:");
+scanf("%d",&number);
+printf("cube of number is:%d "5,);*/
+
+
+
+    printf("You\n \nare\nlearning\n\'c\' language\n\"Do you know C language\"");
+
+       const float PI=3.14;
+const float  PI1=4;
+    printf("The value of PI is: %f,",PI,PI1);
+
+return 0;
+}
